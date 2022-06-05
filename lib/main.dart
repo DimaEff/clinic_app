9b55e84a99cf2382f935000130a4816a343db978
DimaEff <69797190+DimaEff@users.noreply.github.com>
2022-06-05 15:43:47 +0300

    init app

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(ClinicApp());
+
+class ClinicApp extends StatelessWidget {
+  const ClinicApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ClinicApp',
+      home: HomePage(),
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+        backgroundColor: Colors.white,
+        textTheme: TextTheme(
+          bodyMedium: TextStyle(color: Colors.black),
+        )
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).backgroundColor,
+        appBar: AppBar(title: Text('Clinic'), leading: Icon(Icons.medication_outlined),),
+        body: Text('Clinic'),
+      ),
+    );
+  }
+}
+
